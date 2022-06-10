54c295977d8b0bc69dee472e4053c2ad687376cc
Ridey Madhok <rideymadhok@gmail.com>
2022-06-09 18:46:55 -0700

    Express Install

--- .vs/app.js
+++
@@ -1,8 +0,0 @@
-const express = requires ('express');
-const app = express();
-
-app.app.get('/', (req, res) => {
-  res.send('GET request to the homepage')
-});
-
-app.listen(3000, () => console.log('Listening on port 3000...'));

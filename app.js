954b25b798f7ecfca02d8f4a6730c7d22fd05b11
Ridey Madhok <rideymadhok@gmail.com>
2022-06-09 19:32:38 -0700

    Lernt how to handle get and post request

--- app.js
+++ app.js
@@ -1,9 +1,27 @@
-const express = require('express')
+const express = require('express');
+const { parse } = require('path/posix');
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+const courses = [
+  {id: 1, Name: 'Math'},
+  {id: 2, Name: 'English'},
+  {id: 3, Name: 'Science'}
+]
+
+app.get('/api/courses/:id', (req, res) => {
+  let course = courses.find(c => c.id === parseInt(req.params.id));
+  if (!course) res.status(404).send('Course with the given id not found');
+  res.send(course);
+})
+
+app.post('/api/course', (req, res) => {
+  let course = {
+    id: courses.length + 1,
+    name: res.body.name
+  }
+  courses.push(course);
+  res.send(course);
 })
 
 app.listen(port, () => {

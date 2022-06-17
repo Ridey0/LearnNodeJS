70e3e69126f617e4b0419a74241469d4fca1db95
Ridey Madhok <madhokridey@gmail.com>
2022-06-16 23:48:57 -0700

    Leaning Middleware

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require('express');
+const logger = require('./logger');
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
+app.use(logger);
 
 const courses = [
   {id: 1, name: 'Math'},

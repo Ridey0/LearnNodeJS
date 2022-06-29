abc8eda8599c34e23c96f94e9ffef792625e05c2
Ridey Madhok <madhokridey@gmail.com>
2022-06-28 19:07:07 -0700

    Proceding to Learn Middleware

--- app.js
+++ app.js
@@ -2,7 +2,9 @@ const express = require('express');
 const logger = require('./logger');
 const helmet = require('helmet');
 const morgan = require('morgan');
+const config = require('config');
 const app = express();
+const startupDebugger = require('debug')('app:startup');
 const port = process.env.PORT || 3000;
 
 app.use(helmet());
@@ -10,9 +12,14 @@ app.use(express.json());
 app.use(logger);
 app.use(express.static('public'));
 
+//Configrations
+
+console.log('Application Name: ' + config.get('name'));
+console.log('Mail Server: ' + config.get('mail.password'));
+
 if (app.get('env') === 'development') {
   app.use(morgan('tiny'));
-  console.log('Morgan is enabled...')
+  startupDebugger('Morgan is enabled...');
 }
 
 const courses = [
